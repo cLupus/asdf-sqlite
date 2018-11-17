289230ac0f58886ba69af06603c58c1764b44295
Sindre Nistad <sindre_123@msn.com>
2018-11-17 22:26:21 +0100

    feat: Added required scripts

---
+++ bin/utils.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+export SQLITE_URL="https://www.sqlite.org"
+#export SQLITE_HISTORY_URL="${SQLITE_URL}/chronology.html"
+
+export WORKING_DIR="/tmp/asdf-sqlite"
+mkdir -p ${WORKING_DIR}
+
+# Borrowed from https://github.com/rbenv/ruby-build/pull/631/files#diff-fdcfb8a18714b33b07529b7d02b54f1dR942
+function sort_versions() {
+  sed 'h; s/[+-]/./g; s/.p\([[:digit:]]\)/.z\1/; s/$/.z/; G; s/\n/ /' | \
+    LC_ALL=C sort -t. -k 1,1 -k 2,2n -k 3,3n -k 4,4n -k 5,5n | awk '{print $2}'
+}
+
+# Borrowed from https://github.com/danhper/asdf-python/blob/master/bin/utils.sh
+echoerr() {
+  printf "\033[0;31m%s\033[0m" "$1" >&2
+}
+
+get_sqlite_url () {
+    curl --silent ${SQLITE_URL}/$1
+}
