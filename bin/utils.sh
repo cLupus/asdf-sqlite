24a842afb33fe150aa7c4e624f3bba3c2f99bc6d
Sindre Nistad <sindre_123@msn.com>
2022-10-12 20:55:52 +0200

    feat: Add shfmt

--- bin/utils.sh
+++ bin/utils.sh
@@ -6,15 +6,15 @@ export SQLITE_URL="https://www.sqlite.org"
 
 # Borrowed from https://github.com/rbenv/ruby-build/pull/631/files#diff-fdcfb8a18714b33b07529b7d02b54f1dR942
 function sort_versions() {
-  sed 'h; s/[+-]/./g; s/.p\([[:digit:]]\)/.z\1/; s/$/.z/; G; s/\n/ /' | \
-    LC_ALL=C sort -t. -k 1,1 -k 2,2n -k 3,3n -k 4,4n -k 5,5n | awk '{print $2}'
+	sed 'h; s/[+-]/./g; s/.p\([[:digit:]]\)/.z\1/; s/$/.z/; G; s/\n/ /' |
+		LC_ALL=C sort -t. -k 1,1 -k 2,2n -k 3,3n -k 4,4n -k 5,5n | awk '{print $2}'
 }
 
 # Borrowed from https://github.com/danhper/asdf-python/blob/master/bin/utils.sh
 echoerr() {
-  printf "\033[0;31m%s\033[0m" "$1" >&2
+	printf "\033[0;31m%s\033[0m" "$1" >&2
 }
 
-get_sqlite_url () {
-    curl --silent "$SQLITE_URL/$1"
+get_sqlite_url() {
+	curl --silent "$SQLITE_URL/$1"
 }
--- scripts/shellcheck.bash
+++ scripts/shellcheck.bash
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 
 exec shellcheck -s bash -x \
-  bin/* -P lib/
-  
+	bin/* -P lib/
